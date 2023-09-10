32c365648da5afc5b06f594f5fd0b59274d4c563
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-09-10 10:26:27 +0530

    df

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import './index.css'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 import TodoMaker from './components/TodoMaker'
 
 function App() {
